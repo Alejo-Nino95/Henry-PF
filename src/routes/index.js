7c34eca427daa7548e1998318729a438a78b6a1e
FDDeC <fededido089@gmail.com>
2022-03-18 23:34:22 -0300

    emails listos para usar

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ const userRoutes = require('./userRoutes/userRoutes.js');
 const productRoutes = require('./productRoutes/productRoutes.js');
 const categoryRoutes = require('./categoryRoutes/categoryRoutes.js');
 const pedidosRoutes = require('./pedidosRoutes/pedidosRoutes.js')
+const emailRoutes = require('./emailRoutes')
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
@@ -13,6 +14,7 @@ router.use('/user', userRoutes);
 router.use('/product', productRoutes);
 router.use('/category', categoryRoutes);
 router.use('/pedidos', pedidosRoutes);
+router.use('/email',emailRoutes)
 
 
 module.exports = router;
